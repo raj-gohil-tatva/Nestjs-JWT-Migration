b674ea2c4593c8166e29dfce7b586d553e4838bd
Raj Gohil <raj.gohil@tatvasoft.com>
2023-05-10 17:16:30 +0530

    Server related changes.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+const startServer = async () => {
+  try {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+    console.log(`🚀 Server is running on🏃‍♂️${await app.getUrl()}`);
+  } catch (error) {
+    console.error(`⚠️ Unable to start the server due to ${error.message}`);
+  }
+};
+
+startServer();
