6e0bb1c088e6aa23c223068c78e16e0e7fefdb29
Raj Gohil <raj.gohil@tatvasoft.com>
2023-05-11 15:14:01 +0530

    Fixed no migration were found issue.

--- src/db/entities/user.entity.ts
+++ src/db/entities/user.entity.ts
@@ -1,5 +1,6 @@
-import { PrimaryGeneratedColumn, Column } from 'typeorm';
+import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
 
+@Entity()
 export class User {
   @PrimaryGeneratedColumn()
   ID: number;
